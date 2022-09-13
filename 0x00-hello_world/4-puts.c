33d11b4a8ea728adf3f07dc9421aa78b691a163b
Emmanuel <emmaimade14@gmail.com>
2022-09-13 00:02:32 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: My first program which makes use of the puts function
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
