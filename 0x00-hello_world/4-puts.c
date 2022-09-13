7cd75d108043cb2db982fd0298254cabddbd906c
Emmanuel <emmaimade14@gmail.com>
2022-09-13 00:04:14 +0000

    Updated Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * Description: My first program which makes use of the puts function
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
