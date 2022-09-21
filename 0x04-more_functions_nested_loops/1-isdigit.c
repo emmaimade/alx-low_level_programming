3fd37b63047714fc1d80e57d6545f84c431044c4
Emmanuel <emmaimade14@gmail.com>
2022-09-21 13:09:34 +0000

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isdigit - Returns 1 if c is a digit and 0 if not.
+* @c: int type variable.
+* Return: 1 if digit and 0 if otherwise
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+	return (0);
+}
