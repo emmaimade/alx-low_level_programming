eb919d20fba775f0788433c4cc0e101ba9661c5f
Emmanuel <emmaimade14@gmail.com>
2022-09-21 12:55:15 +0000

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isupper - Returns 1 if c is uppercase and 0 if not.
+* @c: int type variable.
+* Return: 1 if uppercase and 0 if lowercase.
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+	return (0);
+}
