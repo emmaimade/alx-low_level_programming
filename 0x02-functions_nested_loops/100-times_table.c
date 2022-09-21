b6612dc7686d387171dfca2be06e7a635d21e6e5
Emmanuel <emmaimade14@gmail.com>
2022-09-21 01:12:49 +0000

    Updated 100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,50 @@
 #include "main.h"
 
 /**
-* print_times_table - Print the times table n number of times.
-* @n: The number of times table requested.
-* Return: Nothing.
+* print_times_table - print the n number of times table, starting with 0
+* Description: If n is greater thatn 15 or less than 0, print nothing
+* @n: int type number
 */
+
 void print_times_table(int n)
 {
-	int i, j, res;
+	int x = 0, y, z;
 
-	if (!(n > 15 || n < 0))
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (i = 0; i <= n; i++)
+		for (y = 0; y <= n; y++)
 		{
-			for (j = 0; j <= n; j++)
+			z = x * y;
+			if (z > 99)
 			{
-				res = (i * j);
-				if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				if (res < 10 && j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 10 && res < 100)
-				{
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 100 && j != 0)
-				{
-					_putchar((res / 100) + '0');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
-				}
-				else
-				{
-					_putchar((res % 10) + '0');
-				}
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
 			}
-			_putchar('\n');
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
+		x++;
 	}
 }
